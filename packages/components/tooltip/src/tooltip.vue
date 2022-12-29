024ce0fb6e295175c266c5f1856722295ca3a0ca
huangwantong <huangwantong@escopetech.com>
2022-12-29 15:08:07 +0800

    feat: tooltip

--- packages/components/tooltip/src/tooltip.vue
+++ packages/components/tooltip/src/tooltip.vue
@@ -1,26 +1,33 @@
 <template>
-  <div :class="[n()]" v-on="events">
+  <div ref="tooltipRef" :class="[n()]" v-on="events">
     <slot></slot>
     <Transition name="van-tooltip-fade">
-      <div v-if="visible" :class="[n('content'), `is-` + effect]" :data-side="placement">
+      <div v-if="visible" :class="[n('content'), `is-${effect}`]" :style="contentStyle" :data-side="placement">
         <slot name="content">{{ content }}</slot>
+        <span :class="n('arrow')"></span>
       </div>
     </Transition>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, CSSProperties, watch, onMounted } from 'vue'
 import { createNamespace } from '@vangle/utils'
 import { TooltipProps } from './tooltip'
 defineOptions({
   name: 'VanTooltip'
 })
-
-defineProps(TooltipProps)
-
-const { n, classes } = createNamespace('tooltip')
+const props = defineProps(TooltipProps)
+const tooltipRef = ref<HTMLElement | null>(null)
+const { n } = createNamespace('tooltip')
 const visible = ref(false)
+
+const contentStyle = ref<CSSProperties>({})
+
+watch(() => props.placement, () => {
+  getStyle()
+})
+
 const events = {
   mouseenter() {
     visible.value = true
@@ -29,6 +36,31 @@ const events = {
     visible.value = false
   }
 }
+
+onMounted(() => {
+  getStyle()
+})
+
+function getStyle() {
+  const tooltipRect = tooltipRef.value?.getBoundingClientRect()
+  if (tooltipRect) {
+    const [direction] = props.placement.split('-')
+
+    if (['top', 'bottom'].includes(direction)) {
+      const value = tooltipRect.height + 10
+      const y = direction === 'top' ? -value : value
+      contentStyle.value = {
+        transform: `translate(-50%, ${y}px)`
+      }
+    } else {
+      const value = tooltipRect.width + 10
+      const x = direction === 'left' ? -value : value
+      contentStyle.value = {
+        transform: `translate(${x}px, 50%)`
+      }
+    }
+  }
+} 
 </script>
 
 <style lang="less">
--- packages/docs/zh/component/tooltip.md
+++ packages/docs/zh/component/tooltip.md
@@ -5,12 +5,43 @@ lang: en-US
 
 # Tooltip
 
-Commonly used Tooltip.
+Display prompt information for mouse hover.
 
 ## Basic usage
 
-:::demo description of Tooltip
+Tooltip has 9 placements.
+
+:::demo Use attribute `content` to set the display content when hover. The attribute `placement` determines the position of the tooltip. Its value is `[orientation]` with four orientations `top`, `left`, `right`, `bottom` 
 
 tooltip/basic
+
+:::
+
+## Theme
+
+Tooltip has two built-in themes: `dark` and `light`。
+
+:::tip
+
+To use customized theme, you will have to known where your tooltip is rendered into, if your tooltip is rendered into the root element, you will need to set the css rule globally.
+
+It is recommended that not using linear gradient background color when you using customized theme and showing the arrow at the same time, because the popup arrow and the content are two different elements,
+the popup arrow's style needs to be set individually, and when it comes to the gradient background color, it might seem a little bit weird.
+
+:::
+
+:::demo Set `effect` to modify theme, and the default value is `dark`.
+
+tooltip/theme
+
+:::
+
+## More Content
+
+Display multiple lines of text and set their format.
+
+:::demo Override attribute `content` of `el-tooltip` by adding a slot named `content`.
+
+tooltip/rich-content
 
 :::
