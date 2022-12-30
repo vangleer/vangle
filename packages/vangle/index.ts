1bd3a73531b000ceec0a3e08fcf9fc0f73ded2c9
huangwantong <huangwantong@escopetech.com>
2022-12-30 14:49:34 +0800

    fix: 单独导出

--- packages/vangle/index.ts
+++ packages/vangle/index.ts
@@ -1,24 +1,7 @@
 export * from '../components'
 
 import installer from './defaults'
+export * from '@vangle/components'
 
 export const install = installer.install
 export default installer
-
-// export * from '../components'
-// import type { App, Plugin } from 'vue'
-
-// import { VanButton } from '@vangle/components'
-
-// const components = [VanButton] as Plugin[]
-
-// const INSTALLED_KEY = Symbol('INSTALLED_KEY')
-// export const install = (app: App) => {
-//   if (app[INSTALLED_KEY]) return
-//   app[INSTALLED_KEY] = true
-//   components.forEach(comp => app.use(comp))
-// }
-
-// export default {
-//   install
-// }
