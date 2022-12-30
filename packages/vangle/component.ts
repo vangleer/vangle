2fc574239f5a3bf470b410d8acdb90314676fe30
huangwantong <huangwantong@escopetech.com>
2022-12-30 11:51:33 +0800

    build: 打包

--- packages/vangle/component.ts
+++ packages/vangle/component.ts
@@ -1,4 +1,4 @@
 import type { Plugin } from 'vue'
-import { VanButton } from '@vangle/components'
+import { VanButton, VanIcon, VanDialog, VanMessage, VanSlider, VanTooltip, ripple as VanRipple } from '@vangle/components'
 
-export default [VanButton] as Plugin[]
+export default [VanButton, VanIcon, VanDialog, VanMessage, VanSlider, VanTooltip, VanRipple] as Plugin[]
