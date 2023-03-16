a12ef683b20973d5655be9aacc497e1c9b104c9d
huangwantong <huangwantong@escopetech.com>
2023-03-16 16:11:11 +0800

    chore(tag): 标签

---
+++ packages/docs/examples/tag/basic.vue
@@ -0,0 +1,7 @@
+<template>
+  <van-tag>Tag 1</van-tag>
+  <van-tag class="ml-2" type="success">Tag 2</van-tag>
+  <van-tag class="ml-2" type="info">Tag 3</van-tag>
+  <van-tag class="ml-2" type="warning">Tag 4</van-tag>
+  <van-tag class="ml-2" type="danger">Tag 5</van-tag>
+</template>
---
+++ packages/docs/examples/tag/rounded.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="row">
+    <van-tag
+      v-for="item in items"
+      :key="item.label"
+      :type="item.type"
+      class="mx-1"
+      effect="dark"
+      round
+    >
+      {{ item.label }}
+    </van-tag>
+  </div>
+  <div class="row">
+    <van-tag
+      v-for="item in items"
+      :key="item.label"
+      :type="item.type"
+      class="mx-1"
+      effect="light"
+      round
+    >
+      {{ item.label }}
+    </van-tag>
+  </div>
+  <div class="row">
+    <van-tag
+      v-for="item in items"
+      :key="item.label"
+      :type="item.type"
+      class="mx-1"
+      effect="plain"
+      round
+    >
+      {{ item.label }}
+    </van-tag>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import type { TagType } from '@vangle/components'
+
+type Item = { type: TagType; label: string }
+
+const items = ref<Array<Item>>([
+  { type: '', label: 'Tag 1' },
+  { type: 'success', label: 'Tag 2' },
+  { type: 'info', label: 'Tag 3' },
+  { type: 'danger', label: 'Tag 4' },
+  { type: 'warning', label: 'Tag 5' },
+])
+</script>
+<style>
+.row {
+  margin-bottom: 10px;
+}
+</style>
